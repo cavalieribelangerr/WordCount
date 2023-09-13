ac57659a46b3cc099c2a8f89fe1143300e1484a5
cavalieribelangerr <143746287+cavalieribelangerr@users.noreply.github.com>
2023-09-13 11:41:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # Romain Cavalieri-Bélanger
 # Word_Count, 2023
 
+# compter le nombre de mots, s: str -> nb: int
 def count_word():
-  s = input("entre la phrase: ")
   nb = len(s.split())
-  print(nb)
   return nb
-
-count_word()
